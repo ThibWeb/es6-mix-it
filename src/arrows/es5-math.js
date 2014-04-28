413c21336fbb01c5dcf1e7252b57126146dc4e9f
Thibaud Colas <thibaudcolas@gmail.com>
2014-04-29 00:54:23 +0200

    First example bis: arrow functions

---
+++ src/arrows/es5-math.js
@@ -0,0 +1,7 @@
+var arr = [5, 10, 42];
+
+var square = function (x) {
+  return x * x;
+};
+
+console.log(arr.map(square));
---
+++ src/arrows/es6-math.js
@@ -0,0 +1,5 @@
+var arr = [5, 10, 42];
+
+var square = x => x * x;
+
+console.log(arr.map(square));
